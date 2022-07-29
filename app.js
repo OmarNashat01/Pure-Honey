36853c82d3145dcfdd8d183a2a566b9e046c56b6
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-07-29 15:12:44 +0200

    2

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Setup CORS
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
     return res.status(200).json({});
   }
   next();
-});
+}); */
 
 app.use((req, res, next) => {
   console.log({ body: req.body });
