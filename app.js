5edbe47c98ba0e5902a33010477bdca8f5e6a182
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-08-01 14:44:49 +0200

    3

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000',"https://pure-honey.herokuapp.com"],
     credentials: true,
     methods:["GET","POST","DELETE","PATCH"]
   }
