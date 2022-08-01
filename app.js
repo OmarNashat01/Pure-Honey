065faa60dbecc07e95b8f08bcef80a9cf801aac2
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-08-01 23:42:34 +0200

    1

--- app.js
+++ app.js
@@ -68,16 +68,11 @@ app.use(bodyParser.json())
 
 
 app.use((req, res, next) => {
+ 
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
 }); 
 
 app.use((req, res, next) => {
